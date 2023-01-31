1859af719033f7baf9d7ce25d9a0f32cdfa496de
Maaz Ahmad Khan <maaz95734@gmail.com>
2023-01-31 20:38:09 +0530

    bug fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
 import { AyatComponent } from "./components/ayat.component";
 import "./App.css";
+import { useEffectOnce } from "./app/hooks/use-effect-once";
 export const App = () => {
+  useEffectOnce(() => {
+    setInterval(() => {
+      window.location.reload();
+    }, 15000);
+  });
   return <AyatComponent />;
 };
